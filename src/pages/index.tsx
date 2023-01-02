61d82309727fa69660dce2d99f32f360aade225f
ThamyrisSantana <thamyrissatana3@gmail.com>
2023-01-02 14:14:31 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.new</title>
+        <title>Home | ig.new</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
 
-      <h1>Oi oi</h1>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding illustration" />
+      </main>
     </>
   );
 }
