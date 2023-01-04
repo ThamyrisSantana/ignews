2d0182387a7ff936418e18e7946a913e68c857e7
ThamyrisSantana <thamyrissatana3@gmail.com>
2023-01-03 21:34:16 -0300

    feat: add next-auth and faunadb configurations to project

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      scope: "read:user",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, emails, credentials }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("users_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("users_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
+export default NextAuth(authOptions);
